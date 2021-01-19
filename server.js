fcc370efb179b496eba1a1f4fb06ea022d5c8762
Flukeznpm <nattapong.meun@gmail.com>
2021-01-19 11:17:40 +0700

    init

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cookieSession = require('cookie-session');
+const port = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2'],
+    maxAge: 4800 * 1000
+}));
+
+const loginRouter = require('./routes/login');
+const registerRouter = require('./routes/register');
+const profileRouter = require('./routes/profile');
+const roomRouter = require('./routes/room');
+const logoutRouter = require('./routes/logout');
+
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
+app.use('/profile', profileRouter);
+app.use('/room', roomRouter);
+app.use('/logout', logoutRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>No path of your request!</h1>')
+});
+
+app.listen(port, () => {
+    console.log(`>>> Server is running on port : ${port} <<<`)
+});
+
+module.exports = app;
